ecebf483c5bd1a5b9bfe4cf3bf1f4419f0d80f03
Gowtham Rao <rao@ohdsi.org>
2022-08-07 17:17:00 -0400

    Updated tests

--- R/ModifyCohorts.R
+++ R/ModifyCohorts.R
@@ -14,10 +14,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-#' Modify cohort(s)
+#' Modify cohort
 #'
 #' @description
-#' Modify cohort(s) by censoring, padding, limiting cohorts periods.
+#' Modify cohort by censoring, padding, limiting cohorts periods.
 #' Censoring: Provide a date for right, left, both censoring. All cohorts will be truncated to the given date.
 #' Pad days: Add days to either cohort start or cohort end dates. Maybe negative numbers. Final cohort will not be outside the persons observation period.
 #' Limit cohort periods: Filter the cohorts to a given date range of cohort start, or cohort end or both.
@@ -30,7 +30,9 @@
 #'
 #' @template CohortDatabaseSchema
 #'
-#' @template OldToNewCohortId
+#' @template OldCohortId
+#'
+#' @template NewCohortId
 #'
 #' @template PurgeConflicts
 #'
@@ -55,37 +57,30 @@
 #'
 #' @return
 #' NULL
-modifyCohorts <- function(connectionDetails = NULL,
-                          connection = NULL,
-                          cohortDatabaseSchema = NULL,
-                          cohortTable = "cohort",
-                          oldToNewCohortId,
-                          cohortStartCensorDate = NULL,
-                          cohortEndCensorDate = NULL,
-                          cohortStartFilterRange = NULL,
-                          cohortEndFilterRange = NULL,
-                          cohortStartPadDays = 0,
-                          cohortEndPadDays = 0,
-                          cdmDatabaseSchema = NULL,
-                          tempEmulationSchema = getOption("sqlRenderTempEmulationSchema"),
-                          purgeConflicts = FALSE) {
+modifyCohort <- function(connectionDetails = NULL,
+                         connection = NULL,
+                         cohortDatabaseSchema = NULL,
+                         cohortTable = "cohort",
+                         oldCohortId,
+                         newCohortId = oldCohortId,
+                         cohortStartCensorDate = NULL,
+                         cohortEndCensorDate = NULL,
+                         cohortStartFilterRange = NULL,
+                         cohortEndFilterRange = NULL,
+                         cohortStartPadDays = NULL,
+                         cohortEndPadDays = NULL,
+                         cdmDatabaseSchema = NULL,
+                         tempEmulationSchema = getOption("sqlRenderTempEmulationSchema"),
+                         purgeConflicts = TRUE) {
   errorMessages <- checkmate::makeAssertCollection()
-  checkmate::assertDataFrame(x = oldToNewCohortId,
-                             min.rows = 1,
-                             add = errorMessages)
-  checkmate::assertNames(
-    x = colnames(oldToNewCohortId),
-    must.include = c("oldCohortId", "newCohortId"),
-    add = errorMessages
-  )
   checkmate::assertIntegerish(
-    x = oldToNewCohortId$oldCohortId,
+    x = oldCohortId,
     min.len = 1,
     null.ok = FALSE,
     add = errorMessages
   )
   checkmate::assertIntegerish(
-    x = oldToNewCohortId$newCohortId,
+    x = newCohortId,
     min.len = 1,
     null.ok = FALSE,
     add = errorMessages
@@ -131,12 +126,6 @@ modifyCohorts <- function(connectionDetails = NULL,
     null.ok = TRUE,
     add = errorMessages
   )
-  checkmate::assertArray(
-    x = cohortStartFilterRange,
-    any.missing = FALSE,
-    null.ok = TRUE,
-    add = errorMessages
-  )
   checkmate::assertDate(
     x = cohortStartFilterRange,
     any.missing = TRUE,
@@ -145,12 +134,9 @@ modifyCohorts <- function(connectionDetails = NULL,
     null.ok = TRUE,
     add = errorMessages
   )
-  checkmate::assertArray(
-    x = cohortEndFilterRange,
-    any.missing = TRUE,
-    null.ok = TRUE,
-    add = errorMessages
-  )
+  if (!is.null(cohortStartFilterRange)) {
+    checkmate::assert_true(x = cohortStartFilterRange[1] <= cohortStartFilterRange[2])
+  }
   checkmate::assertDate(
     x = cohortEndFilterRange,
     any.missing = TRUE,
@@ -159,16 +145,19 @@ modifyCohorts <- function(connectionDetails = NULL,
     null.ok = TRUE,
     add = errorMessages
   )
+  if (!is.null(cohortEndFilterRange)) {
+    checkmate::assert_true(x = cohortEndFilterRange[1] <= cohortEndFilterRange[2])
+  }
   checkmate::assertIntegerish(
     x = cohortStartPadDays,
     len = 1,
-    null.ok = FALSE,
+    null.ok = TRUE,
     add = errorMessages
   )
   checkmate::assertIntegerish(
     x = cohortEndPadDays,
     len = 1,
-    null.ok = FALSE,
+    null.ok = TRUE,
     add = errorMessages
   )
   
@@ -191,29 +180,29 @@ modifyCohorts <- function(connectionDetails = NULL,
     on.exit(DatabaseConnector::disconnect(connection))
   }
   
-  cohortIdsInCohortTable <-
-    getCohortIdsInCohortTable(
-      connection = connection,
-      cohortDatabaseSchema = cohortDatabaseSchema,
-      cohortTable = cohortTable,
-      tempEmulationSchema = tempEmulationSchema
-    )
-  
-  conflicitingCohortIdsInTargetCohortTable <-
-    intersect(x = oldToNewCohortId$newCohortId %>% unique(),
-              y = cohortIdsInCohortTable %>% unique())
-  
-  performPurgeConflicts <- FALSE
-  if (length(conflicitingCohortIdsInTargetCohortTable) > 0) {
-    if (purgeConflicts) {
-      performPurgeConflicts <- TRUE
-    } else {
-      stop(
-        paste0(
-          "The following cohortIds already exist in the target cohort table, causing conflicts :",
-          paste0(conflicitingCohortIdsInTargetCohortTable, collapse = ",")
+  if (oldCohortId != newCohortId) {
+    if (!purgeConflicts) {
+      cohortIdsInCohortTable <-
+        getCohortIdsInCohortTable(
+          connection = connection,
+          cohortDatabaseSchema = cohortDatabaseSchema,
+          cohortTable = cohortTable,
+          tempEmulationSchema = tempEmulationSchema
         )
-      )
+      conflicitingCohortIdsInTargetCohortTable <-
+        intersect(x = newCohortId,
+                  y = cohortIdsInCohortTable %>% unique())
+      
+      if (length(conflicitingCohortIdsInTargetCohortTable) > 0) {
+        stop(
+          paste0(
+            "The following cohortIds already exist in the target cohort table, causing conflicts :",
+            paste0(newCohortId,
+                   collapse = ",")
+          )
+        )
+        
+      }
     }
   }
   
@@ -223,83 +212,266 @@ modifyCohorts <- function(connectionDetails = NULL,
   
   copyCohortsToTempTable(
     connection = connection,
-    oldToNewCohortId = oldToNewCohortId,
+    oldToNewCohortId = dplyr::tibble(oldCohortId = oldCohortId, newCohortId = newCohortId),
     sourceCohortDatabaseSchema = cohortDatabaseSchema,
     sourceCohortTable = cohortTable,
     targetCohortTable = tempTable1
   )
   
-  sqlEraFy <- " DROP TABLE IF EXISTS @temp_table_2;
+  ## Cohort Censor Start Date -----
+  if (!is.null(cohortStartCensorDate)) {
+    sql <- "  DROP TABLE IF EXISTS @temp_table_2;
+          	SELECT cohort_definition_id,
+              	  subject_id,
+              		CASE
+              			WHEN DATEFROMPARTS(@year_cohort_censor_start,
+              			                    @month_cohort_censor_start,
+              			                    @day_cohort_censor_start) >= cohort_start_date
+              				THEN DATEFROMPARTS(@year_cohort_censor_start,
+              			                    @month_cohort_censor_start,
+              			                    @day_cohort_censor_start)
+              			ELSE cohort_start_date
+              			END AS cohort_start_date,
+              		cohort_end_date
+          	INTO @temp_table_2
+          	FROM @temp_table_1;
 
- ; "
+          	DROP TABLE IF EXISTS @temp_table_1;
+
+          SELECT *
+          INTO @temp_table_1
+          FROM @temp_table_2
+          WHERE cohort_start_date <= cohort_end_date;
+
+          DROP TABLE IF EXISTS @temp_table_2;
+  "
+    
+    DatabaseConnector::renderTranslateExecuteSql(
+      connection = connection,
+      sql = sql,
+      profile = FALSE,
+      progressBar = FALSE,
+      reportOverallTime = FALSE,
+      tempEmulationSchema = tempEmulationSchema,
+      year_cohort_censor_start = clock::get_year(cohortStartCensorDate),
+      month_cohort_censor_start = clock::get_month(cohortStartCensorDate),
+      day_cohort_censor_start = clock::get_day(cohortStartCensorDate),
+      temp_table_1 = tempTable1,
+      temp_table_2 = tempTable2
+    )
+  }
   
-  DatabaseConnector::renderTranslateExecuteSql(
-    connection = connection,
-    sql = sqlEraFy,
-    profile = FALSE,
-    progressBar = FALSE,
-    reportOverallTime = FALSE,
-    tempEmulationSchema = tempEmulationSchema,
-    eraconstructorpad = eraconstructorpad,
-    cdm_database_schema = cdmDatabaseSchema,
-    temp_table_1 = tempTable1,
-    temp_table_2 = tempTable2
-  )
+  ## Cohort Censor End Date -----
+  if (!is.null(cohortEndCensorDate)) {
+    sql <- "  DROP TABLE IF EXISTS @temp_table_2;
+          	SELECT cohort_definition_id,
+          	        subject_id,
+          	        cohort_start_date,
+              		CASE
+              			WHEN DATEFROMPARTS(@year_cohort_censor_end,
+              			                    @month_cohort_censor_end,
+              			                    @day_cohort_censor_end) <= cohort_end_date
+              				THEN DATEFROMPARTS(@year_cohort_censor_end,
+              			                    @month_cohort_censor_end,
+              			                    @day_cohort_censor_end)
+              			ELSE cohort_end_date
+              			END AS cohort_end_date
+          	INTO @temp_table_2
+          	FROM @temp_table_1;
+
+          	DROP TABLE IF EXISTS @temp_table_1;
+
+          SELECT *
+          INTO @temp_table_1
+          FROM @temp_table_2
+          WHERE cohort_start_date <= cohort_end_date;
+
+          DROP TABLE IF EXISTS @temp_table_2;
+  "
+    
+    DatabaseConnector::renderTranslateExecuteSql(
+      connection = connection,
+      sql = sql,
+      profile = FALSE,
+      progressBar = FALSE,
+      reportOverallTime = FALSE,
+      tempEmulationSchema = tempEmulationSchema,
+      year_cohort_censor_end = clock::get_year(cohortEndCensorDate),
+      month_cohort_censor_end = clock::get_month(cohortEndCensorDate),
+      day_cohort_censor_end = clock::get_day(cohortEndCensorDate),
+      temp_table_1 = tempTable1,
+      temp_table_2 = tempTable2
+    )
+  }
   
-  cohortIdsToDeleteFromSource <- oldToNewCohortId %>%
-    dplyr::filter(.data$oldCohortId == .data$newCohortId) %>%
-    dplyr::pull(.data$oldCohortId)
   
-  if (length(cohortIdsToDeleteFromSource) > 0) {
+  ## Cohort Start Filter Range -----
+  if (!is.null(cohortStartFilterRange)) {
+    sql <- "  DROP TABLE IF EXISTS @temp_table_2;
+          	SELECT cohort_definition_id,
+              	  subject_id,
+              		cohort_start_date,
+              		cohort_end_date
+          	INTO @temp_table_2
+          	FROM @temp_table_1
+          	WHERE cohort_start_date >= DATEFROMPARTS( @year_cohort_censor_start_low,
+                            			                    @month_cohort_censor_start_low,
+                            			                    @day_cohort_censor_start_low) AND
+          	      cohort_start_date <= DATEFROMPARTS( @year_cohort_censor_start_high,
+                            			                    @month_cohort_censor_start_high,
+                            			                    @day_cohort_censor_start_high);
+
+
+          	DROP TABLE IF EXISTS @temp_table_1;
+
+          SELECT *
+          INTO @temp_table_1
+          FROM @temp_table_2
+          WHERE cohort_start_date <= cohort_end_date;
+
+          DROP TABLE IF EXISTS @temp_table_2;
+  "
+    
+    DatabaseConnector::renderTranslateExecuteSql(
+      connection = connection,
+      sql = sql,
+      profile = FALSE,
+      progressBar = FALSE,
+      reportOverallTime = FALSE,
+      tempEmulationSchema = tempEmulationSchema,
+      year_cohort_censor_start_low = clock::get_year(min(cohortStartFilterRange)),
+      month_cohort_censor_start_low = clock::get_month(min(cohortStartFilterRange)),
+      day_cohort_censor_start_low = clock::get_day(min(cohortStartFilterRange)),
+      year_cohort_censor_start_high = clock::get_year(max(cohortStartFilterRange)),
+      month_cohort_censor_start_high = clock::get_month(max(cohortStartFilterRange)),
+      day_cohort_censor_start_high = clock::get_day(max(cohortStartFilterRange)),
+      temp_table_1 = tempTable1,
+      temp_table_2 = tempTable2
+    )
+  }
+  
+  ## Cohort End Filter Range -----
+  if (!is.null(cohortEndFilterRange)) {
+    sql <- "  DROP TABLE IF EXISTS @temp_table_2;
+          	SELECT cohort_definition_id,
+              	  subject_id,
+              	  cohort_start_date,
+              	  cohort_end_date
+          	INTO @temp_table_2
+          	FROM @temp_table_1
+          	WHERE cohort_end_date >= DATEFROMPARTS( @year_cohort_censor_end_low,
+                            			                    @month_cohort_censor_end_low,
+                            			                    @day_cohort_censor_end_low) AND
+          	      cohort_end_date <= DATEFROMPARTS( @year_cohort_censor_end_high,
+                            			                    @month_cohort_censor_end_high,
+                            			                    @day_cohort_censor_end_high);
+
+          	DROP TABLE IF EXISTS @temp_table_1;
+
+          SELECT *
+          INTO @temp_table_1
+          FROM @temp_table_2
+          WHERE cohort_start_date <= cohort_end_date;
+
+          DROP TABLE IF EXISTS @temp_table_2;
+  "
+    
+    DatabaseConnector::renderTranslateExecuteSql(
+      connection = connection,
+      sql = sql,
+      profile = FALSE,
+      progressBar = FALSE,
+      reportOverallTime = FALSE,
+      tempEmulationSchema = tempEmulationSchema,
+      year_cohort_censor_end_low = clock::get_year(min(cohortEndFilterRange)),
+      month_cohort_censor_end_low = clock::get_month(min(cohortEndFilterRange)),
+      day_cohort_censor_end_low = clock::get_day(min(cohortEndFilterRange)),
+      year_cohort_censor_end_high = clock::get_year(max(cohortEndFilterRange)),
+      month_cohort_censor_end_high = clock::get_month(max(cohortEndFilterRange)),
+      day_cohort_censor_end_high = clock::get_day(max(cohortEndFilterRange)),
+      temp_table_1 = tempTable1,
+      temp_table_2 = tempTable2
+    )
+  }
+  
+  ## Cohort Pad -----
+  if (any(!is.null(cohortStartPadDays), !is.null(cohortEndPadDays))) {
+    sql <- "  DROP TABLE IF EXISTS @temp_table_2;
+          	SELECT cohort_definition_id,
+              	  subject_id,
+              	  {@cohort_start_pad_days != ''} ? {DATEADD(DAY, @cohort_start_pad_days, cohort_start_date)} ? {@cohort_start_date},
+              	  {@cohort_end_pad_days != ''} ? {DATEADD(DAY, @cohort_end_pad_days, cohort_end_date)} ? {@cohort_end_date}
+          	INTO @temp_table_2
+          	FROM @temp_table_1;
+
+          	DROP TABLE IF EXISTS @temp_table_1;
+
+          SELECT *
+          INTO @temp_table_1
+          FROM @temp_table_2
+          WHERE cohort_start_date <= cohort_end_date;
+
+          DROP TABLE IF EXISTS @temp_table_2;
+  "
+    
+    DatabaseConnector::renderTranslateExecuteSql(
+      connection = connection,
+      sql = sql,
+      profile = FALSE,
+      progressBar = FALSE,
+      reportOverallTime = FALSE,
+      tempEmulationSchema = tempEmulationSchema,
+      cohort_start_pad_days = cohortStartPadDays,
+      cohort_end_pad_days = cohortEndPadDays,
+      temp_table_1 = tempTable1,
+      temp_table_2 = tempTable2
+    )
+    
+    eraFyCohorts(
+      connection = connection,
+      oldToNewCohortId = dplyr::tibble(oldCohortId = oldCohortId,
+                                       newCohortId = newCohortId),
+      cdmDatabaseSchema = cdmDatabaseSchema,
+      tempEmulationSchema = tempEmulationSchema,
+      cohortTable = tempTable1,
+      cohortDatabaseSchema = NULL
+    )
+  }
+  
+  if (oldCohortId != newCohortId) {
     ParallelLogger::logTrace(
       paste0(
         "The following cohortIds will be deleted from your cohort table and \n",
         " replaced with ear fy'd version of those cohorts using the same original cohort id: ",
-        paste0(cohortIdsToDeleteFromSource, collapse = ",")
+        paste0(newCohortId, collapse = ",")
       )
     )
-    deleteCohortRecords(
-      connection = connection,
-      cohortDatabaseSchema = cohortDatabaseSchema,
-      cohortTable = cohortTable,
-      cohortIds = cohortIdsToDeleteFromSource
-    )
   }
+  deleteCohortRecords(
+    connection = connection,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = cohortTable,
+    cohortIds = newCohortId
+  )
   
-  if (performPurgeConflicts) {
-    ParallelLogger::logTrace(
-      paste0(
-        "The following conflicting cohortIds will be deleted from your cohort table \n",
-        " as part resolving conflicts: ",
-        paste0(conflicitingCohortIdsInTargetCohortTable, collapse = ",")
-      )
-    )
-    deleteCohortRecords(
-      connection = connection,
-      cohortDatabaseSchema = cohortDatabaseSchema,
-      cohortTable = cohortTable,
-      cohortIds = conflicitingCohortIdsInTargetCohortTable
-    )
-  }
   DatabaseConnector::renderTranslateExecuteSql(
     connection = connection,
     sql = " INSERT INTO {@cohort_database_schema != ''} ? {@cohort_database_schema.@cohort_table} : {@cohort_table}
             SELECT cohort_definition_id, subject_id, cohort_start_date, cohort_end_date
-            FROM @temp_table_2;",
+            FROM @temp_table_1;",
     profile = FALSE,
     progressBar = FALSE,
     reportOverallTime = FALSE,
     cohort_database_schema = cohortDatabaseSchema,
     tempEmulationSchema = tempEmulationSchema,
     cohort_table = cohortTable,
-    temp_table_2 = tempTable2
+    temp_table_1 = tempTable1
   )
   
   DatabaseConnector::renderTranslateExecuteSql(
     connection = connection,
     sql = " DROP TABLE IF EXISTS @temp_table_1;
-            DROP TABLE IF EXISTS @temp_table_2;
-            DROP TABLE IF EXISTS #old_to_new_cohort_id;",
+            DROP TABLE IF EXISTS @temp_table_2;",
     profile = FALSE,
     progressBar = FALSE,
     reportOverallTime = FALSE,
---
+++ tests/testthat/test-modifyCohorts.R
@@ -0,0 +1,149 @@
+testthat::test_that("Testing Modify cohorts", {
+  # generate unique name for a cohort table
+  sysTime <- as.numeric(Sys.time()) * 100000
+  tableName <- paste0("cr", sysTime)
+  tempTableName <- paste0("#", tableName, "_1")
+
+  # make up date for a cohort table
+  # this cohort table will have two subjects * two cohorts, within the same cohort
+  cohort <- dplyr::tibble(
+    cohortDefinitionId = c(1, 3, 3),
+    subjectId = c(1, 3, 3),
+    cohortStartDate = c(as.Date("1999-01-01"), as.Date("2010-01-01"), as.Date("1999-01-15")),
+    cohortEndDate = c(as.Date("1999-01-31"), as.Date("2010-01-05"), as.Date("1999-01-25"))
+  )
+
+  # upload table
+  connection <-
+    DatabaseConnector::connect(connectionDetails = connectionDetails)
+  DatabaseConnector::insertTable(
+    connection = connection,
+    databaseSchema = cohortDatabaseSchema,
+    tableName = tableName,
+    data = cohort,
+    dropTableIfExists = TRUE,
+    createTable = TRUE,
+    tempTable = FALSE,
+    camelCaseToSnakeCase = TRUE,
+    progressBar = FALSE
+  )
+  
+  CohortAlgebra:::modifyCohort(
+    connection = connection,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = tableName,
+    oldCohortId = 1, 
+    newCohortId = 2,
+    cohortStartCensorDate = as.Date("1999-01-05"), 
+    cohortEndCensorDate = as.Date("1999-01-25")
+  )
+  
+  cohortExpected <- dplyr::tibble(
+    cohortDefinitionId = c(2),
+    subjectId = c(1),
+    cohortStartDate = c(as.Date("1999-01-05")),
+    cohortEndDate = c(as.Date("1999-01-25"))
+  )
+  
+  cohortObserved <-
+    DatabaseConnector::renderTranslateQuerySql(
+      connection = connection,
+      sql = paste0(
+        "SELECT * FROM @cohort_database_schema.@table_name
+        WHERE cohort_definition_id = 2
+        order by cohort_definition_id, subject_id, cohort_start_date;"
+      ),
+      cohort_database_schema = cohortDatabaseSchema,
+      table_name = tableName,
+      snakeCaseToCamelCase = TRUE
+    ) %>%
+    dplyr::tibble()
+  
+  testthat::expect_equal(
+    object = cohortObserved %>%
+      nrow(),
+    expected = 1
+  )
+  testthat::expect_true(object = all.equal(target = cohortExpected, current = cohortObserved))
+  
+
+  # test for range date start ----
+  CohortAlgebra:::modifyCohort(
+    connection = connection,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = tableName,
+    oldCohortId = 3, 
+    newCohortId = 2,
+    cohortStartFilterRange = c(as.Date("1998-01-01"), as.Date("1999-12-31")), 
+    purgeConflicts = TRUE
+  )
+  
+  cohortExpected <- dplyr::tibble(
+    cohortDefinitionId = c(2),
+    subjectId = c(3),
+    cohortStartDate = c(as.Date("1999-01-15")),
+    cohortEndDate = c(as.Date("1999-01-25"))
+  )
+  
+  cohortObserved <-
+    DatabaseConnector::renderTranslateQuerySql(
+      connection = connection,
+      sql = paste0(
+        "SELECT * FROM @cohort_database_schema.@table_name
+        WHERE cohort_definition_id = 2
+        order by cohort_definition_id, subject_id, cohort_start_date;"
+      ),
+      cohort_database_schema = cohortDatabaseSchema,
+      table_name = tableName,
+      snakeCaseToCamelCase = TRUE
+    ) %>%
+    dplyr::tibble()
+  
+  testthat::expect_equal(
+    object = cohortObserved %>%
+      nrow(),
+    expected = 1
+  )
+  testthat::expect_true(object = all.equal(target = cohortExpected, current = cohortObserved))
+  
+  
+  
+  # test for range date end ----
+  CohortAlgebra:::modifyCohort(
+    connection = connection,
+    cohortDatabaseSchema = cohortDatabaseSchema,
+    cohortTable = tableName,
+    oldCohortId = 3, 
+    newCohortId = 2,
+    cohortEndFilterRange = c(as.Date("2010-01-01"), as.Date("2010-01-09")), 
+    purgeConflicts = TRUE
+  )
+  
+  cohortExpected <- dplyr::tibble(
+    cohortDefinitionId = c(2),
+    subjectId = c(3),
+    cohortStartDate = as.Date("2010-01-01"),
+    cohortEndDate = as.Date("2010-01-05")
+  )
+  
+  cohortObserved <-
+    DatabaseConnector::renderTranslateQuerySql(
+      connection = connection,
+      sql = paste0(
+        "SELECT * FROM @cohort_database_schema.@table_name
+        WHERE cohort_definition_id = 2
+        order by cohort_definition_id, subject_id, cohort_start_date;"
+      ),
+      cohort_database_schema = cohortDatabaseSchema,
+      table_name = tableName,
+      snakeCaseToCamelCase = TRUE
+    ) %>%
+    dplyr::tibble()
+  
+  testthat::expect_equal(
+    object = cohortObserved %>%
+      nrow(),
+    expected = 1
+  )
+  testthat::expect_true(object = all.equal(target = cohortExpected, current = cohortObserved))
+})
