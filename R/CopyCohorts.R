d7fe94601e52be1ffceb513b779cdc664fa6440e
Gowtham Rao <rao@ohdsi.org>
2023-01-06 17:36:22 -0500

    Package maintenance

--- R/CopyCohorts.R
+++ R/CopyCohorts.R
@@ -21,6 +21,8 @@
 #'
 #' `r lifecycle::badge("stable")`
 #'
+#' @template ConnectionDetails
+#'
 #' @template Connection
 #'
 #' @template OldToNewCohortId
@@ -65,9 +67,11 @@ copyCohorts <- function(connectionDetails = NULL,
                         purgeConflicts = FALSE,
                         tempEmulationSchema = getOption("sqlRenderTempEmulationSchema")) {
   errorMessages <- checkmate::makeAssertCollection()
-  checkmate::assertDataFrame(x = oldToNewCohortId,
-                             min.rows = 1,
-                             add = errorMessages)
+  checkmate::assertDataFrame(
+    x = oldToNewCohortId,
+    min.rows = 1,
+    add = errorMessages
+  )
   checkmate::assertNames(
     x = colnames(oldToNewCohortId),
     must.include = c("oldCohortId", "newCohortId"),
@@ -120,7 +124,7 @@ copyCohorts <- function(connectionDetails = NULL,
     add = errorMessages
   )
   checkmate::reportAssertions(collection = errorMessages)
-  
+
   if (all(
     (sourceCohortDatabaseSchema == targetCohortDatabaseSchema),
     (sourceCohortTable == targetCohortTable)
@@ -138,7 +142,7 @@ copyCohorts <- function(connectionDetails = NULL,
     connection <- DatabaseConnector::connect(connectionDetails)
     on.exit(DatabaseConnector::disconnect(connection))
   }
-  
+
   DatabaseConnector::insertTable(
     connection = connection,
     tableName = "#old_to_new_cohort_id",
@@ -151,7 +155,7 @@ copyCohorts <- function(connectionDetails = NULL,
     camelCaseToSnakeCase = TRUE,
     data = oldToNewCohortId
   )
-  
+
   if (purgeConflicts) {
     deleteCohortRecords(
       connection = connection,
@@ -168,11 +172,13 @@ copyCohorts <- function(connectionDetails = NULL,
         cohortTable = targetCohortTable,
         tempEmulationSchema = tempEmulationSchema
       )
-    
+
     conflicitingCohortIdsInTargetCohortTable <-
-      intersect(x = oldToNewCohortId$newCohortId %>% unique() %>% sort(),
-                y = cohortIdsInCohortTable %>% unique())
-    
+      intersect(
+        x = oldToNewCohortId$newCohortId %>% unique() %>% sort(),
+        y = cohortIdsInCohortTable %>% unique()
+      )
+
     if (length(conflicitingCohortIdsInTargetCohortTable) > 0) {
       stop(
         paste0(
@@ -182,7 +188,7 @@ copyCohorts <- function(connectionDetails = NULL,
       )
     }
   }
-  
+
   sql <- SqlRender::loadRenderTranslateSql(
     sqlFilename = "CopyCohorts.sql",
     packageName = utils::packageName(),
@@ -200,7 +206,7 @@ copyCohorts <- function(connectionDetails = NULL,
     progressBar = FALSE,
     reportOverallTime = FALSE
   )
-  
+
   eraFyCohorts(
     connection = connection,
     cohortDatabaseSchema = targetCohortDatabaseSchema,
