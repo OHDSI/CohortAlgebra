fb4c4843bffc0a23f54724f6daba3ce94179c251
Gowtham Rao <rao@ohdsi.org>
2023-09-10 21:33:37 -0400

    Clean up and make cran compatible changes
    * Delete unused files
    * update description and spell check
    * Package maintenance

--- tests/testthat/test-unionCohorts.R
+++ tests/testthat/test-unionCohorts.R
@@ -3,7 +3,7 @@ testthat::test_that("Testing cohort union", {
   sysTime <- as.numeric(Sys.time()) * 100000
   tableName <- paste0("cr", sysTime)
   tempTableName <- paste0("#", tableName, "_1")
-  
+
   # make up date for a cohort table
   cohort <- dplyr::tibble(
     cohortDefinitionId = c(1, 2, 2),
@@ -19,7 +19,7 @@ testthat::test_that("Testing cohort union", {
       as.Date("2022-12-30")
     )
   )
-  
+
   # upload table
   connection <-
     DatabaseConnector::connect(connectionDetails = connectionDetails)
@@ -36,7 +36,7 @@ testthat::test_that("Testing cohort union", {
   )
   # disconnecting - as this is a test for a non temp cohort table
   DatabaseConnector::disconnect(connection)
-  
+
   # should not throw error
   unionCohorts(
     connectionDetails = connectionDetails,
@@ -51,7 +51,7 @@ testthat::test_that("Testing cohort union", {
     tempEmulationSchema = tempEmulationSchema,
     purgeConflicts = FALSE
   )
-  
+
   # extract the generated output and compare to expected
   connection <-
     DatabaseConnector::connect(connectionDetails = connectionDetails)
@@ -68,10 +68,12 @@ testthat::test_that("Testing cohort union", {
       snakeCaseToCamelCase = TRUE
     ) |>
     dplyr::tibble()
-  
-  testthat::expect_equal(object = nrow(dataPostUnion),
-                         expected = 2) # union logic should collapse to 2 rows
-  
+
+  testthat::expect_equal(
+    object = nrow(dataPostUnion),
+    expected = 2
+  ) # union logic should collapse to 2 rows
+
   # create the expected output data frame object to compare
   cohortExpected <- dplyr::tibble(
     cohortDefinitionId = c(3, 3),
@@ -79,9 +81,9 @@ testthat::test_that("Testing cohort union", {
     cohortStartDate = c(as.Date("2022-01-01"), as.Date("2022-08-15")),
     cohortEndDate = c(as.Date("2022-05-10"), as.Date("2022-12-30"))
   )
-  
+
   testthat::expect_true(object = all(dataPostUnion == cohortExpected))
-  
+
   testthat::expect_error(
     unionCohorts(
       connectionDetails = connectionDetails,
@@ -97,7 +99,7 @@ testthat::test_that("Testing cohort union", {
       isTempTable = TRUE
     )
   )
-  
+
   unionCohorts(
     connection = connection,
     sourceCohortDatabaseSchema = cohortDatabaseSchema,
@@ -111,7 +113,7 @@ testthat::test_that("Testing cohort union", {
     targetCohortTable = paste0("#", tableName, "2"),
     isTempTable = TRUE
   )
-  
+
   DatabaseConnector::renderTranslateExecuteSql(
     connection = connection,
     sql = "DROP TABLE IF EXISTS @cohort_database_schema.@table_temp;
@@ -123,6 +125,6 @@ testthat::test_that("Testing cohort union", {
     progressBar = FALSE,
     reportOverallTime = FALSE
   )
-  
+
   DatabaseConnector::disconnect(connection = connection)
 })
